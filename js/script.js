fff507988bfcb7b14ddd9d7039a76e54cf137406
Kim Elkjær Marcher <kije@eaaa.dk>
2019-02-07 09:56:42 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var blooper = document.getElementById("myVideo");
+
+window.onscroll = function() {
+    var offsetTop = blooper.getBoundingClientRect().top;
+    var offsetBottom = blooper.getBoundingClientRect().bottom; 
+    if (offsetTop <= 50 && offsetTop > 0) {
+       playVideo();
+    } else if (offsetBottom < 0) {
+        stopVideo();
+    }
+}
+
+function playVideo() {
+    blooper.play();
+}
+
+function stopVideo() {
+    blooper.pause();
+}
+
